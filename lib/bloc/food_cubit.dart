348831620f9c1c56b964cdf8bbfb04aaf38023eb
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2022-12-16 07:00:04 +0200

    add get meals method inn cubit

--- lib/bloc/food_cubit.dart
+++ lib/bloc/food_cubit.dart
@@ -2,10 +2,14 @@
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:food_rate/bloc/food_states.dart';
+import 'package:food_rate/data/service/dio.dart';
 
 class FoodCubit extends Cubit<FoodStates>  {
 
   FoodCubit():super(FoodInitialState());
 
-
+late DioHelper dioHelper;
+void getMeals(){
+  dioHelper.getDate(url: 'meals/');
+}
 }
