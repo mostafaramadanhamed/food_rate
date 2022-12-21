731da901a94719018804d9a1fb1391412a7aa44f
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2022-12-21 06:45:42 +0200

    add meal item

---
+++ lib/Presentation/home/widgets/meal_item.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:food_rate/data/models/meal_model.dart';
+class MealItem extends StatelessWidget {
+  final Meal meal;
+  const MealItem({Key? key, required this.meal}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsetsDirectional.fromSTEB(8, 8, 8, 8),
+      padding:const EdgeInsetsDirectional.all(4.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(8),
+        color: Colors.white.withOpacity(.7),
+      ),
+      child: InkWell(
+        onTap: (){
+          Navigator.pushNamed(context, mealDetailsScreen, arguments: meal);
+        },
+        child: GridTile(
+          footer: Hero(
+            tag: meal.charId,
+            child: Container(
+              width: double.infinity,
+              padding:const EdgeInsets.symmetric(vertical: 10.0,horizontal: 15.0,),
+              color: Colors.black.withOpacity(0.3),
+              alignment: Alignment.bottomCenter,
+              child: Text(meal.name, style: const TextStyle(
+                height: 1.3,
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+                overflow: TextOverflow.ellipsis,
+                maxLines: 2,
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          child: meal.img.isNotEmpty?
+          FadeInImage(
+            width: double.infinity,
+            height: double.infinity,
+            placeholder:  const AssetImage('assets/images/loading.gif'),
+            image: NetworkImage(meal.img),fit: BoxFit.cover,)
+              :Image.asset('assets/images/null.jpg'),
+        ),
+      ),
+    );
+  }
+}
